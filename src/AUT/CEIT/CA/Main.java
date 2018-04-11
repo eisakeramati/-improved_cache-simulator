d36d14bc50d22469cc3f0f208784c5a2140650dd
eisa keramati <eisa.keramati@gmail.com>
2018-04-11 16:27:34 +0430

    second commit

--- src/AUT/CEIT/CA/Main.java
+++ src/AUT/CEIT/CA/Main.java
@@ -5,7 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
 
-        long a = Long.parseLong("10000000000000000000000000000000",2);
-        System.out.println(a);
+//        long a = Long.parseLong("10000000000000000000000000000000",2);
+//        System.out.println(a);
+//
+//        for (int i = 0; i < 5; i++) {
+//            i++;
+//            System.out.println(i);
+//        }
+
+        inputCreator inputCreator = new inputCreator();
+        for (int i = 0; i <inputCreator.getFianl_arr().length ; i++) {
+            if (i==100)
+                System.out.println("donedonedonedonedonedonedonedonedonedonedonedone");
+            System.out.println(inputCreator.getFianl_arr()[i]);
+        }
     }
 }
--- src/AUT/CEIT/CA/inputCreator.java
+++ src/AUT/CEIT/CA/inputCreator.java
@@ -8,20 +8,21 @@ import java.util.Random;
 public class inputCreator {
 
     private int first_arr[];
-    private int fianl_arr[];
+    private int final_arr[];
 
     public int[] getFianl_arr() {
-        return fianl_arr;
+        return final_arr;
     }
 
     public void setFianl_arr(int[] fianl_arr) {
-        this.fianl_arr = fianl_arr;
+        this.final_arr = fianl_arr;
     }
 
     public inputCreator() {
         first_arr = new int[100];
-        fianl_arr = new int[500];
-
+        final_arr = new int[500];
+        randomSetter();
+        finSetter();
     }
 
     public int[] getFirst_arr() {
@@ -33,20 +34,67 @@ public class inputCreator {
     }
 
     private void randomSetter() {
-        Random random = new Random(2000);
+
 
         for (int i = 0; i < 100; i++) {
-            first_arr[i] = random.nextInt();
-        }
-    }
-   
-    private void finSetter()
-    {
-        for (int i = 0; i < ; i++) {
-            if ()
+            first_arr[i] = randomNumberInRange(0, 2000);
         }
     }
 
-    
+    private void finSetter() {
+        for (int i = 0; i < 100; i++) {
+            final_arr[i] = first_arr[i];
+        }
+
+        int[] arr = new int[4];
+        int temp = 0;
+        int last = 0;
+        int counter = 0;
+        for (int i = 100; i < 500; i++) {
+            if (counter % 4 == 0 || counter % 4 == 2) {
+                int rand = randomNumberInRange(0, 99);
+                final_arr[i] = first_arr[rand];
+                i++;
+                final_arr[i] = first_arr[rand] + 1;
+                i++;
+                final_arr[i] = first_arr[rand] + 2;
+                i++;
+                final_arr[i] = first_arr[rand] + 3;
+                arr[indexArray(arr, last)] = rand;
+                last++;
+                counter++;
+            } else if (counter % 4 == 3) {
+                final_arr[i] = first_arr[arr[temp % 4]];
+                i++;
+                final_arr[i] = first_arr[arr[temp % 4]] + 1;
+                i++;
+                final_arr[i] = first_arr[arr[temp % 4]] + 2;
+                i++;
+                final_arr[i] = first_arr[arr[temp % 4]] + 3;
+                counter++;
+                temp++;
+            } else {
+                final_arr[i] = first_arr[randomNumberInRange(0, 99)];
+                i++;
+                final_arr[i] = first_arr[randomNumberInRange(0, 99)];
+                i++;
+                final_arr[i] = first_arr[randomNumberInRange(0, 99)];
+                i++;
+                final_arr[i] = first_arr[randomNumberInRange(0, 99)];
+                counter++;
+            }
+        }
+    }
+
+
+    public static int randomNumberInRange(int min, int max) {
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+    private int indexArray(int[] given, int last) {
+        return last % 4;
+    }
+
 
 }
